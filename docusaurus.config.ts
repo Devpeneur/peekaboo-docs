4924e4887c87cd33d267912bb6e01a73c950f6ae
Piyush Dhall <contact@piyush.ovh>
2024-05-16 18:49:54 +0530

    change some configs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Peekaboo',
+  tagline: "Relaxing your client's wait time ;)",
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -15,8 +15,11 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Devpeneur', // Usually your GitHub org/user name.
+  projectName: 'peekaboo-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -58,7 +61,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Peekaboo Docs',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -68,11 +71,11 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        {to: '/blog', label: 'FAQ', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/Devpeneur/peekaboo-docs',
           label: 'GitHub',
           position: 'right',
         },
@@ -85,7 +88,7 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Docs',
               to: '/docs/intro',
             },
           ],
@@ -121,7 +124,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Peekaboo. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
